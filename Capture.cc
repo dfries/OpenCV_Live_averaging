a3994ca80567c32b1e7dc8cae9f0f9321b0e0883
David Fries <David@Fries.net>
2014-10-18 23:06:36 -0500

    remove the delay that didn't make a difference

--- Capture.cc
+++ Capture.cc
@@ -5,7 +5,6 @@
 #include <iostream>
 #include <ctype.h>
 #include <stdlib.h>
-#include <time.h>
 
 using namespace std;
 using namespace cv;
@@ -35,8 +34,6 @@ Capture::Capture(int argc, char **argv) :
 	// appears to not update until waitKey or if it has a thread
 	//startWindowThread();
 	Mat capture, image;
-	struct timespec req = {0, 200*1000*1000};
-	int msleep = 0;
 	for(;;)
 	{
 		Input >> capture;
@@ -50,32 +47,11 @@ Capture::Capture(int argc, char **argv) :
 		char c = waitKey(1);
 		if(c != -1)
 		{
-			cout << c << flush;
-			if(isdigit(c))
-			{
-				int d = c - '0';
-				if(msleep)
-					msleep*=10;
-				msleep += d;
-				cout << "msleep " << msleep << endl;
-			}
-			else
-			{
-				cout << "using msleep " << msleep << endl;
-				req.tv_sec = msleep / 1000;
-				req.tv_nsec = (msleep%1000)*1000*1000;
-				msleep = 0;
-
-				cout << Input.get(CV_CAP_PROP_FPS) << "fps"
-					<< endl;
-				cout << Input.get(CV_CAP_PROP_FOURCC) << "4c"
-					<< endl;
-				cout << "gain " << Input.get(CV_CAP_PROP_GAIN)
-					<< endl;
-				cout << "exposure "
-					<< Input.get(CV_CAP_PROP_EXPOSURE)
-					<< endl;
-			}
+			cout << Input.get(CV_CAP_PROP_FPS) << "fps" << endl;
+			cout << Input.get(CV_CAP_PROP_FOURCC) << "4c" << endl;
+			cout << "gain " << Input.get(CV_CAP_PROP_GAIN) << endl;
+			cout << "exposure " << Input.get(CV_CAP_PROP_EXPOSURE)
+				<< endl;
 			if(c == 'f')
 			{
 				cout << "set fps "
@@ -83,6 +59,5 @@ Capture::Capture(int argc, char **argv) :
 					<< endl;
 			}
 		}
-		nanosleep(&req, NULL);
 	}
 }
