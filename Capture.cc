067e41ecfa0f77c3abe12ca9c85b4a613df90b62
David Fries <David@Fries.net>
2014-10-18 23:03:39 -0500

    try reading frames slower and getting camera parameters
    As expected just reading frames less often doesn't cause the camera
    to keep the shutter open for a longer period of time (figuratively
    speaking).

--- Capture.cc
+++ Capture.cc
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <ctype.h>
 #include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 using namespace cv;
@@ -32,8 +33,10 @@ Capture::Capture(int argc, char **argv) :
 	}
 	namedWindow(CapWin);
 	// appears to not update until waitKey or if it has a thread
-	startWindowThread();
+	//startWindowThread();
 	Mat capture, image;
+	struct timespec req = {0, 200*1000*1000};
+	int msleep = 0;
 	for(;;)
 	{
 		Input >> capture;
@@ -41,5 +44,45 @@ Capture::Capture(int argc, char **argv) :
 			break;
 		imshow(CapWin, capture);
 		//capture.copyTo(image);
+
+		// stupid no give me what you have without waiting, don't use
+		// OpenCV GUI code in anything but developing tests
+		char c = waitKey(1);
+		if(c != -1)
+		{
+			cout << c << flush;
+			if(isdigit(c))
+			{
+				int d = c - '0';
+				if(msleep)
+					msleep*=10;
+				msleep += d;
+				cout << "msleep " << msleep << endl;
+			}
+			else
+			{
+				cout << "using msleep " << msleep << endl;
+				req.tv_sec = msleep / 1000;
+				req.tv_nsec = (msleep%1000)*1000*1000;
+				msleep = 0;
+
+				cout << Input.get(CV_CAP_PROP_FPS) << "fps"
+					<< endl;
+				cout << Input.get(CV_CAP_PROP_FOURCC) << "4c"
+					<< endl;
+				cout << "gain " << Input.get(CV_CAP_PROP_GAIN)
+					<< endl;
+				cout << "exposure "
+					<< Input.get(CV_CAP_PROP_EXPOSURE)
+					<< endl;
+			}
+			if(c == 'f')
+			{
+				cout << "set fps "
+					<< Input.set(CV_CAP_PROP_FPS, 5)
+					<< endl;
+			}
+		}
+		nanosleep(&req, NULL);
 	}
 }
