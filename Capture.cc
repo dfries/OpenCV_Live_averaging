9ee3b19452d89ef16ca992e2840c814b2b8eb596
David Fries <David@Fries.net>
2016-04-27 20:27:29 -0500

    refactor to class members
    This fixes a problem where clear would use convert_in, but in the
    rolling average case it was now the last image, and so it would
    reset to an old image instead of the current one.

--- Capture.cc
+++ Capture.cc
@@ -3,7 +3,6 @@
 #include "Capture.h"
 
 #include <iostream>
-#include <deque>
 
 #include <ctype.h>
 #include <stdlib.h>
@@ -13,7 +12,10 @@ using namespace cv;
 
 Capture::Capture(int argc, char **argv) :
 	CapWin("Unprocessed Image"),
-	OutWin("Output Image")
+	OutWin("Output Image"),
+	Count(0),
+	Brightness(1),
+	FramePos(0)
 {
 	// take a number for a device name or a file, but it seems like
 	// the file won't accept a video device filename
@@ -38,127 +40,126 @@ Capture::Capture(int argc, char **argv) :
 	// appears to not update until waitKey or if it has a thread
 	//startWindowThread();
 	Mat capture;
-	Input >> capture;
-	// the sum of all captured images
-	Mat accum = Mat::zeros(capture.rows, capture.cols, CV_32SC3);
-	// used to type convert the captured image before processing
-	Mat convert_in(accum.rows, accum.cols, accum.type());
-	Mat convert_out(accum.rows, accum.cols, accum.type());
 	Mat out;
+	Input >> capture;
 
-	// keep track of past frames to subtract from the accumulated image
-	deque<Mat> frames;
-	size_t pos = 0;
-	
+	Accum = Mat::zeros(capture.rows, capture.cols, CV_32SC3);
+	ConvertIn = Accum.clone();
+	cv::Mat convert_out = Accum.clone();
+
 	capture.copyTo(out);
-	int count = 0;
-	double multiplier = 1;
 	for(;;)
 	{
 		Input >> capture;
 		if(capture.empty())
 			break;
 		imshow(CapWin, capture);
-		capture.convertTo(convert_in, convert_in.type());
+		capture.convertTo(ConvertIn, ConvertIn.type());
 
-		if(frames.empty())
+		// process while ConvertIn is still the last frame read
+		ProcessKey();
+
+		if(Frames.empty())
 		{
-			add(convert_in, accum, accum);
-			++count;
+			add(ConvertIn, Accum, Accum);
+			++Count;
 		}
 		else
 		{
 			// out with the old, in with the new
-			subtract(accum, frames[pos], accum);
-			add(convert_in, accum, accum);
+			subtract(Accum, Frames[FramePos], Accum);
+			add(ConvertIn, Accum, Accum);
 
 			// store off the frame
-			Mat t = frames[pos];
-			frames[pos] = convert_in;
-			convert_in = t;
-			pos = (pos + 1) % frames.size();
+			Mat t = Frames[FramePos];
+			Frames[FramePos] = ConvertIn;
+			ConvertIn = t;
+			FramePos = (FramePos + 1) % Frames.size();
 		}
 
-		accum.convertTo(convert_out, convert_out.type(),
-			multiplier/count);
+		Accum.convertTo(convert_out, convert_out.type(),
+			Brightness/Count);
 		convert_out.convertTo(out, out.type());
 		imshow(OutWin, out);
-
-		// stupid no give me what you have without waiting, don't use
-		// OpenCV GUI code in anything but developing tests
-		char c = waitKey(1);
-		if(c != -1)
-		{
-			/*
-			cout << Input.get(CV_CAP_PROP_FPS) << "fps" << endl;
-			cout << Input.get(CV_CAP_PROP_FOURCC) << "4c" << endl;
-			cout << "gain " << Input.get(CV_CAP_PROP_GAIN) << endl;
-			cout << "exposure " << Input.get(CV_CAP_PROP_EXPOSURE)
-				<< endl;
-			*/
-			if(isdigit(c))
-			{
-				multiplier = c - '0';
-				if(multiplier == 0)
-					multiplier = .5;
-				cout << "Brightness " << multiplier << endl;
-			}
-			else if(c == '+')
-			{
-				if(frames.empty())
-					accum.setTo(cv::Scalar(0, 0, 0));
-
-				// add in the current frame a second time
-				// so the division works out
-				frames.push_back(convert_in.clone());
-				add(convert_in, accum, accum);
-				count = frames.size();
-			}
-			else if(c == '-')
-			{
-				if(!frames.empty())
-				{
-					// subtract out the frame being dropped
-					subtract(accum, frames[pos], accum);
-					frames.erase(frames.begin()+pos);
-				}
-				count = frames.size();
-				if(frames.empty())
-					pos = 0;
-				else
-					pos = pos % frames.size();
-			}
-			else if(c == 'f')
-			{
-				cout << "set fps "
-					<< Input.set(CV_CAP_PROP_FPS, 5)
-					<< endl;
-			}
-			else if(c == 'c')
-			{
-				cout << "clear accumulation matrix count was "
-					<< count << endl;
-				// clear the matrix
-				cv::Scalar zero(0, 0, 0);
-				accum.setTo(zero);
-				for(size_t i = 0; i < frames.size(); ++i)
-				{
-					convert_in.copyTo(frames[i]);
-					accum += convert_in;
-				}
-				if(frames.empty())
-					count = 0;
-			}
-			else if(c == '?')
-			{
-				cout << "0-9	brightness multiplier\n"
-					"c	clear frame\n"
-					"f	set fps to 5 (not working)\n"
-					"+	increment rolling average\n"
-					"-	decrement rolling average\n";
-			}
-			cout << (frames.empty() ? "accum" : "frame") <<
-				" count " << count << endl;
-		}
 	}
+}
+
+void Capture::ProcessKey()
+{
+	// stupid no give me what you have without waiting, don't use
+	// OpenCV GUI code in anything but developing tests
+	char c = waitKey(1);
+	if(c == -1)
+		return;
+
+	/*
+	cout << Input.get(CV_CAP_PROP_FPS) << "fps" << endl;
+	cout << Input.get(CV_CAP_PROP_FOURCC) << "4c" << endl;
+	cout << "gain " << Input.get(CV_CAP_PROP_GAIN) << endl;
+	cout << "exposure " << Input.get(CV_CAP_PROP_EXPOSURE)
+		<< endl;
+	*/
+	if(isdigit(c))
+	{
+		Brightness = c - '0';
+		if(Brightness == 0)
+			Brightness = .5;
+		cout << "Brightness " << Brightness << endl;
+	}
+	else if(c == '+' || c == '=')
+	{
+		if(Frames.empty())
+			Accum.setTo(cv::Scalar(0, 0, 0));
+
+		// add in the current frame a second time
+		// so the division works out
+		Frames.push_back(ConvertIn.clone());
+		add(ConvertIn, Accum, Accum);
+		Count = Frames.size();
+	}
+	else if(c == '-')
+	{
+		if(!Frames.empty())
+		{
+			// subtract out the frame being dropped
+			subtract(Accum, Frames[FramePos], Accum);
+			Frames.erase(Frames.begin()+FramePos);
+		}
+		Count = Frames.size();
+		if(Frames.empty())
+			FramePos = 0;
+		else
+			FramePos = FramePos % Frames.size();
+	}
+	else if(c == 'f')
+	{
+		cout << "set fps "
+			<< Input.set(CV_CAP_PROP_FPS, 5)
+			<< endl;
+	}
+	else if(c == 'c')
+	{
+		cout << "clear Accumulation matrix Count was "
+			<< Count << endl;
+		// clear the matrix
+		cv::Scalar zero(0, 0, 0);
+		Accum.setTo(zero);
+		for(size_t i = 0; i < Frames.size(); ++i)
+		{
+			ConvertIn.copyTo(Frames[i]);
+			Accum += ConvertIn;
+		}
+		if(Frames.empty())
+			Count = 0;
+	}
+	else if(c == '?')
+	{
+		cout << "0-9	brightness Brightness\n"
+			"c	clear frame\n"
+			"f	set fps to 5 (not working)\n"
+			"+ or =	increment rolling average\n"
+			"-	decrement rolling average\n";
+	}
+	cout << (Frames.empty() ? "Accum" : "frame") <<
+		" count " << Count << endl;
 }
--- Capture.h
+++ Capture.h
@@ -3,12 +3,28 @@
 #include "opencv2/highgui/highgui.hpp"
 
 #include <string>
+#include <deque>
 
 class Capture
 {
 public:
 	Capture(int argc, char **argv);
 private:
+	void ProcessKey();
+
 	cv::VideoCapture Input;
 	const std::string CapWin, OutWin;
+
+	// the sum of all captured images
+	cv::Mat Accum;
+	// used to type convert the captured image before processing
+	cv::Mat ConvertIn;
+
+	// number of frames in Accum
+	int Count;
+	double Brightness;
+
+	// keep track of past frames to subtract from the Accumulated image
+	std::deque<cv::Mat> Frames;
+	size_t FramePos;
 };
