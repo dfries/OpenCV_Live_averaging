3ab182efe44a6457da6f0d6e49117473a83f3f6e
David Fries <David@Fries.net>
2016-04-27 20:05:52 -0500

    implement a rolling average mode

--- Capture.cc
+++ Capture.cc
@@ -3,6 +3,8 @@
 #include "Capture.h"
 
 #include <iostream>
+#include <deque>
+
 #include <ctype.h>
 #include <stdlib.h>
 
@@ -40,8 +42,14 @@ Capture::Capture(int argc, char **argv) :
 	// the sum of all captured images
 	Mat accum = Mat::zeros(capture.rows, capture.cols, CV_32SC3);
 	// used to type convert the captured image before processing
-	Mat convert(accum.rows, accum.cols, accum.type());
+	Mat convert_in(accum.rows, accum.cols, accum.type());
+	Mat convert_out(accum.rows, accum.cols, accum.type());
 	Mat out;
+
+	// keep track of past frames to subtract from the accumulated image
+	deque<Mat> frames;
+	size_t pos = 0;
+	
 	capture.copyTo(out);
 	int count = 0;
 	double multiplier = 1;
@@ -51,13 +59,29 @@ Capture::Capture(int argc, char **argv) :
 		if(capture.empty())
 			break;
 		imshow(CapWin, capture);
+		capture.convertTo(convert_in, convert_in.type());
 
-		capture.convertTo(convert, convert.type());
-		add(convert, accum, accum);
-		++count;
+		if(frames.empty())
+		{
+			add(convert_in, accum, accum);
+			++count;
+		}
+		else
+		{
+			// out with the old, in with the new
+			subtract(accum, frames[pos], accum);
+			add(convert_in, accum, accum);
 
-		accum.convertTo(convert, convert.type(), multiplier/count);
-		convert.convertTo(out, out.type());
+			// store off the frame
+			Mat t = frames[pos];
+			frames[pos] = convert_in;
+			convert_in = t;
+			pos = (pos + 1) % frames.size();
+		}
+
+		accum.convertTo(convert_out, convert_out.type(),
+			multiplier/count);
+		convert_out.convertTo(out, out.type());
 		imshow(OutWin, out);
 
 		// stupid no give me what you have without waiting, don't use
@@ -72,25 +96,69 @@ Capture::Capture(int argc, char **argv) :
 			cout << "exposure " << Input.get(CV_CAP_PROP_EXPOSURE)
 				<< endl;
 			*/
-			cout << "accum count " << count << endl;
 			if(isdigit(c))
 			{
 				multiplier = c - '0';
+				if(multiplier == 0)
+					multiplier = .5;
+				cout << "Brightness " << multiplier << endl;
 			}
-			if(c == 'f')
+			else if(c == '+')
+			{
+				if(frames.empty())
+					accum.setTo(cv::Scalar(0, 0, 0));
+
+				// add in the current frame a second time
+				// so the division works out
+				frames.push_back(convert_in.clone());
+				add(convert_in, accum, accum);
+				count = frames.size();
+			}
+			else if(c == '-')
+			{
+				if(!frames.empty())
+				{
+					// subtract out the frame being dropped
+					subtract(accum, frames[pos], accum);
+					frames.erase(frames.begin()+pos);
+				}
+				count = frames.size();
+				if(frames.empty())
+					pos = 0;
+				else
+					pos = pos % frames.size();
+			}
+			else if(c == 'f')
 			{
 				cout << "set fps "
 					<< Input.set(CV_CAP_PROP_FPS, 5)
 					<< endl;
 			}
-			if(c == 'c')
+			else if(c == 'c')
 			{
-				cout << "clear accumulation matrix\n";
-				// Seems like there should be a better way to
-				// clear a matrix.
-				subtract(accum, accum, accum);
-				count = 0;
+				cout << "clear accumulation matrix count was "
+					<< count << endl;
+				// clear the matrix
+				cv::Scalar zero(0, 0, 0);
+				accum.setTo(zero);
+				for(size_t i = 0; i < frames.size(); ++i)
+				{
+					convert_in.copyTo(frames[i]);
+					accum += convert_in;
+				}
+				if(frames.empty())
+					count = 0;
 			}
+			else if(c == '?')
+			{
+				cout << "0-9	brightness multiplier\n"
+					"c	clear frame\n"
+					"f	set fps to 5 (not working)\n"
+					"+	increment rolling average\n"
+					"-	decrement rolling average\n";
+			}
+			cout << (frames.empty() ? "accum" : "frame") <<
+				" count " << count << endl;
 		}
 	}
 }
